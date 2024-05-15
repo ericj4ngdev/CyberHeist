b3ed076e4003eeb665c780edcc25f0a6a10da23d
ericj4ngdev <ericjangdev@gmail.com>
2024-05-16 01:25:05 +0900

    Fixed NearWall in Covered

--- Source/CyberHeist/Character/CHCharacterPlayer.cpp
+++ Source/CyberHeist/Character/CHCharacterPlayer.cpp
@@ -1036,7 +1036,7 @@ void ACHCharacterPlayer::StopSprint()
 void ACHCharacterPlayer::OnNearWall(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if(bCovered) return;
+	// if(bCovered) return;
 
 	if(CurrentWeapon)
 	{		
